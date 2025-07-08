74af24ebb8fdae01df9129e76aa56d198fd93aa7
Utso Roy <royutso003@gmail.com>
2025-07-08 12:34:18 +0600

    first commt

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthProvider = () => {
+    return (
+        <div>
+            This is Auth Provider
+        </div>
+    );
+};
+
+export default AuthProvider;
